aceae33dcd899412e6789d50b0bebe47dfd9743a
linhuaiying <1274875417@qq.com>
2022-04-04 21:59:42 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import request from '@/utils/request.js'   // 记得改为你的路径
 
+Vue.prototype.request = request  // 此处命名为rq,你可以改
 Vue.config.productionTip = false
+Vue.use(ElementUI, { size: 'small', zIndex: 3000 });
+Vue.use(VueAxios,axios);
 
 new Vue({
   router,
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,77 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<el-container style="height: 100%; border: 1px solid #eee">
+  <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+    <el-menu :default-openeds="['1', '3']">
+      <el-submenu index="1">
+        <template slot="title"><i class="el-icon-message"></i>导航一</template>
+        <el-menu-item-group>
+          <el-menu-item index="1-1">用户管理</el-menu-item>
+          <el-menu-item index="1-2">新闻管理</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+    </el-menu>
+  </el-aside>
+  
+  <el-container>
+    <el-header style="text-align: right; font-size: 12px">
+      <el-dropdown>
+        <i class="el-icon-setting" style="margin-right: 15px"></i>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>查看</el-dropdown-item>
+          <el-dropdown-item>新增</el-dropdown-item>
+          <el-dropdown-item>删除</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+      <span>王小虎</span>
+    </el-header>
+    
+    <el-main>
+      <el-table :data="userList">
+        <el-table-column prop="userName" label="用户名" width="140">
+        </el-table-column>
+        <el-table-column prop="nickName" label="昵称" width="120">
+        </el-table-column>
+        <el-table-column prop="sign" label="签名" width="140">
+        </el-table-column>
+        <el-table-column prop="sex" label="性别">
+        </el-table-column>
+      </el-table>
+    </el-main>
+  </el-container>
+</el-container>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+<style>
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
   }
-}
+  
+  .el-aside {
+    color: #333;
+  }
+</style>
+
+<script>
+  import request from '@/utils/request.js'
+  export default {
+    data() {
+      const item = {
+        username: '',
+        nickname: '',
+        sex: '',
+        sign: ''
+      };
+      return {
+        tableData: Array(20).fill(item),
+        userList: null
+      }
+    },
+    mounted () {
+       request.get('/user/getUserList').then(res=>{
+          this.userList = res;
+       })
+    }
+  };
 </script>
